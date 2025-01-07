3d4f541ea2d1f9893d3b1b20f7ed6462f7deaf9b
ETsBrother <etsbrother1@gmail.com>
2025-01-07 08:04:53 -0500

    finally not sick

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
+var animation = "down"
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _process(delta):
+	var x_direction = Input.get_axis("ui_left", "ui_right")
+	var y_direction = Input.get_axis("ui_up", "ui_down")
+	
+	if x_direction != 0 && x_direction >= abs(y_direction):
+		animation = "right"
+	elif x_direction != 0 && x_direction <= -abs(y_direction):
+		animation = "left"
+	elif y_direction > abs(x_direction):
+		animation = "down"
+	elif y_direction < -abs(x_direction):
+		animation = "up"
+		
+	if "idle" not in animation and x_direction == 0 && y_direction == 0:
+		animation = "idle_" + animation
+	
+	animated_sprite.play(animation)
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
