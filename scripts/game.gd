62716e0145a08e13c2312a61a05cabbc1a1372bd
ETsBrother <etsbrother1@gmail.com>
2025-01-20 11:55:53 -0500

    Started working on pause

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,15 +2,15 @@ extends Node2D
 
 func pause():
 	get_tree().paused = true
-	$PauseMenu.show()
+	get_node("PauseMenu/CanvasLayer").show()
 
 func unpause():
-	$PauseMenu.hide()
+	get_node("PauseMenu/CanvasLayer").hide()
 	get_tree().paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	get_node("PauseMenu/CanvasLayer").hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
